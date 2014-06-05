3b01113758c0ab3f2858a1bbcd3dbb7cbca0e88b
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-05 03:31:57 -0400

    Added Util.maybeMember() method for future use

--- src/org/directcode/idea/Util.java
+++ src/org/directcode/idea/Util.java
@@ -3,11 +3,25 @@ package org.directcode.idea;
 import com.intellij.openapi.diagnostic.Logger;
 import org.apache.log4j.Level;
 
+import java.io.File;
+
 public class Util {
     public static final Logger LOG;
 
     static {
         LOG = Logger.getInstance("DirectIDEA");
         LOG.setLevel(Level.ALL);
+    }
+
+    public static boolean maybeMember() {
+        return fileExists(home(".devtools")) || fileExists(home(".directcode"));
+    }
+
+    public static boolean fileExists(String path) {
+        return new File(path).exists();
+    }
+
+    public static String home(String path) {
+        return System.getProperty("user.home") + "/" + path;
     }
 }
