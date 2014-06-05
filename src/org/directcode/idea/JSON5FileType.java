ad42f6f51955a32a5c9aa6cbe65a11a9f3e57c9b
Kenneth Endfinger <kaendfinger@gmail.com>
2014-06-05 03:07:11 -0400

    Add JSON 5 File Type Association

---
+++ src/org/directcode/idea/JSON5FileType.java
@@ -0,0 +1,46 @@
+package org.directcode.idea;
+
+import com.intellij.lang.javascript.JavaScriptFileType;
+import com.intellij.openapi.fileTypes.LanguageFileType;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import javax.swing.*;
+
+public class JSON5FileType extends LanguageFileType {
+
+    public static final JSON5FileType INSTANCE;
+
+    static {
+        INSTANCE = new JSON5FileType();
+    }
+
+    protected JSON5FileType() {
+        super(JavaScriptFileType.INSTANCE.getLanguage());
+        Util.LOG.debug("Created JSON5 File Type");
+    }
+
+    @NotNull
+    @Override
+    public String getName() {
+        return "JSON5";
+    }
+
+    @NotNull
+    @Override
+    public String getDescription() {
+        return "JSON ECMAScript 5 Extension";
+    }
+
+    @NotNull
+    @Override
+    public String getDefaultExtension() {
+        return "json5";
+    }
+
+    @Nullable
+    @Override
+    public Icon getIcon() {
+        return JavaScriptFileType.INSTANCE.getIcon();
+    }
+}
