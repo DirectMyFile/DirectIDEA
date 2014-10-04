8534a313e96fbf0c123f84fe73826ddbb3105ccc
Kenneth Endfinger <kaendfinger@gmail.com>
2014-10-03 20:53:17 -0400

    Added Pubspec File Template

--- src/org/directcode/idea/StartupComponent.java
+++ src/org/directcode/idea/StartupComponent.java
@@ -1,13 +1,25 @@
 package org.directcode.idea;
 
+import com.intellij.ide.fileTemplates.FileTemplate;
+import com.intellij.ide.fileTemplates.FileTemplateManager;
 import com.intellij.openapi.components.ApplicationComponent;
 import org.jetbrains.annotations.NotNull;
+
+import java.io.IOException;
 
 public class StartupComponent implements ApplicationComponent {
 
     @Override
     public void initComponent() {
         Util.debug("Initializing Startup Component");
+
+        FileTemplate template = FileTemplateManager.getInstance().addTemplate("Basic PubSpec", "yaml");
+
+        try {
+            template.setText(Util.toString(StartupComponent.class.getResourceAsStream("templates/default_pubspec.yaml")));
+        } catch (IOException e) {
+            Util.LOG.error("Failed to load the default pubspec template!", e);
+        }
     }
 
     @Override
--- src/org/directcode/idea/Util.java
+++ src/org/directcode/idea/Util.java
@@ -2,9 +2,12 @@ package org.directcode.idea;
 
 import com.intellij.openapi.diagnostic.Logger;
 import org.apache.log4j.Level;
+import org.codehaus.groovy.runtime.DefaultGroovyMethods;
 import org.jetbrains.annotations.NotNull;
 
-import java.io.File;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Util {
     public static final Logger LOG;
@@ -29,5 +32,19 @@ public class Util {
 
     public static void debug(String line) {
         LOG.debug(line);
+    }
+
+    public static String toString(InputStream stream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+        List<String> lines = new ArrayList<String>();
+
+        String line;
+
+        while ((line = reader.readLine()) != null) {
+            lines.add(line);
+        }
+
+        return DefaultGroovyMethods.join((Iterable<String>) lines, System.lineSeparator());
     }
 }
